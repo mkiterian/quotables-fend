e5d92a802867054893854166775a1f5620948140
mkiterian <hmukite@gmail.com>
2019-01-18 02:51:45 +0300

    add login actions

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -11,7 +11,7 @@ export const registerRequest = () => {
 export const registerFailure = error => (
   {
     type: constants.REGISTER_FAILURE,
-    error,
+    payload: error,
   }
 );
 
@@ -31,5 +31,37 @@ export const registerUser = newUser => (
         dispatch(registerSuccess(response.data));
       })
       .catch((error) => { dispatch(registerFailure(error)); });
+  }
+);
+
+export const loginRequest = () => {
+  return {
+    type: constants.LOGIN_REQUEST,
+  };
+}
+
+export const loginFailure = error => (
+  {
+    type: constants.LOGIN_FAILURE,
+    payload: error,
+  }
+);
+
+export const loginSuccess = (user) => {
+  return (
+  {
+    type: constants.LOGIN_SUCCESS,
+    payload: user,
+  }
+);}
+
+export const loginUser = credentials => (
+  (dispatch) => {
+    dispatch(loginRequest());
+    return axios.post(`${config.API_BASE}/users/login`, credentials)
+      .then((response) => {
+        dispatch(loginSuccess(response.data));
+      })
+      .catch((error) => { dispatch(loginFailure(error)); });
   }
 );
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,23 +3,32 @@ import * as constants from "../constants";
 const initialState = {
   isAuthenticated: false,
   user: {},
-  isFetching: false,
+  isFetching: false
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case constants.REGISTER_REQUEST:
+    case constants.LOGIN_REQUEST:
       return {
         ...state,
-        isFetching: true,
+        isFetching: true
       };
     case constants.REGISTER_SUCCESS:
+    case constants.LOGIN_SUCCESS:
       return {
         ...state,
         isFetching: false,
         isAuthenticated: true,
-        user: action.payload,
-      }
+        user: action.payload
+      };
+    case constants.REGISTER_FAILURE:
+    case constants.LOGIN_FAILURE:
+    return {
+      ...state,
+      isAuthenticated: false,
+      error: action.payload,
+    }
     default:
       return state;
   }
