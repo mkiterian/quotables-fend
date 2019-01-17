a3e87affc7c52051b5173179d64d3f7674cd9baf
mkiterian <hmukite@gmail.com>
2019-01-18 00:56:18 +0300

    wire up react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        App
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
