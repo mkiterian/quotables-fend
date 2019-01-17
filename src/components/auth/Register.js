602c4357eb9731f95717b96f38a53964d3856678
mkiterian <hmukite@gmail.com>
2019-01-18 00:50:35 +0300

    add placeholder content for auth components

---
+++ src/components/auth/Register.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+
+export default Register;
